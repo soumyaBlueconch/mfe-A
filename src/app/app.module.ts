d883dc4712ae31c3b44d3e6c4cc1d6b8c726bbb4
soumyak <soumya.kumari@blueconchtech.com>
2021-09-16 13:16:31 +0530

    implementing micro-frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { FlightsModule } from './flights/flights.module';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FlightsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/flights/flights-search/flights-search.component.ts
@@ -0,0 +1,10 @@
+import {Component, ViewChild, ViewContainerRef, Inject, Injector, ComponentFactoryResolver, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-flights-search',
+  templateUrl: './flights-search.component.html'
+})
+export class FlightsSearchComponent {
+
+}
